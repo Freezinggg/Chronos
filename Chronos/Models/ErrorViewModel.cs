8aef31d2ab2d33b7ac2b57003c2c192e4b04165d
Freezinggg <blerubs@gmail.com>
2025-12-08 11:30:26 +0700

    Add project files.

---
+++ Chronos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chronos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
